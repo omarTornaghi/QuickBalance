cf6ee4e9373e5b383567821df877259e9699ab62
omarTornaghi <omar.tor2011@gmail.com>
2021-03-19 15:27:52 +0100

    MainActivity adesso tiene traccia dello stato

--- app/src/main/java/com/example/quickbalance/MainActivity.kt
+++ app/src/main/java/com/example/quickbalance/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.quickbalance.fragments.CreditiFragment
@@ -12,14 +13,37 @@ import com.example.quickbalance.fragments.HomeFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var selectedFragment:Int = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bottom_navigation.selectedItemId = R.id.ic_home
+
         val homeFragment = HomeFragment();
         val creditiFragment = CreditiFragment();
         val debitiFragment = DebitiFragment()
-        creaFragmentCorrente(homeFragment)
+
+        val stato: Int? = savedInstanceState?.getInt("selected")
+        when(stato){
+            null-> {
+                bottom_navigation.selectedItemId = R.id.ic_home
+                creaFragmentCorrente(homeFragment)
+            }
+            1-> {
+                bottom_navigation.selectedItemId = R.id.ic_crediti
+                creaFragmentCorrente(creditiFragment)
+            }
+            2-> {
+                bottom_navigation.selectedItemId = R.id.ic_home
+                creaFragmentCorrente(homeFragment)
+            }
+            3-> {
+                bottom_navigation.selectedItemId = R.id.ic_debiti
+                creaFragmentCorrente(debitiFragment)
+            }
+        }
+
+
+
         /*Gestione bottom navigation menu*/
 
         bottom_navigation.setOnNavigationItemSelectedListener{
@@ -31,18 +55,21 @@ class MainActivity : AppCompatActivity() {
                     //tv.setTextColor(ContextCompat.getColor(this, R.color.colorTab1))
                     bottom_navigation.itemIconTintList = ContextCompat.getColorStateList(this, R.color.nav_bottombar_item_orange)
                     bottom_navigation.itemTextColor = ContextCompat.getColorStateList(this, R.color.nav_bottombar_item_orange)
+                    selectedFragment = 2;
                     true
                 }
                 R.id.ic_crediti->{
                     creaFragmentCorrente(creditiFragment)
                     bottom_navigation.itemIconTintList = ContextCompat.getColorStateList(this, R.color.nav_bottombar_item_green)
                     bottom_navigation.itemTextColor = ContextCompat.getColorStateList(this, R.color.nav_bottombar_item_green)
+                    selectedFragment = 1;
                     true
                 }
                 R.id.ic_debiti->{
                     creaFragmentCorrente(debitiFragment)
                     bottom_navigation.itemIconTintList = ContextCompat.getColorStateList(this, R.color.nav_bottombar_item_red)
                     bottom_navigation.itemTextColor = ContextCompat.getColorStateList(this, R.color.nav_bottombar_item_red)
+                    selectedFragment = 3;
                     true
                 }
                 else -> false
@@ -57,4 +84,13 @@ class MainActivity : AppCompatActivity() {
             commit()
         }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("selected", selectedFragment)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        selectedFragment = savedInstanceState.getInt("selected")
+    }
 }
