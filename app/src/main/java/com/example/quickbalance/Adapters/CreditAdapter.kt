9fde5842f766f9e662b0f09df00f053adfb2b0d8
omarTornaghi <omar.tor2011@gmail.com>
2021-03-25 15:47:43 +0100

    Iniziato implementazione cards espansibili

--- app/src/main/java/com/example/quickbalance/Adapters/CreditAdapter.kt
+++ app/src/main/java/com/example/quickbalance/Adapters/CreditAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
+import com.example.quickbalance.Animations.AnimationUtils
 import com.example.quickbalance.DataTypes.CreditType
 import com.example.quickbalance.R
 import kotlinx.android.synthetic.main.card_crediti.view.*
@@ -25,7 +26,7 @@ class CreditAdapter(private val crediti: ArrayList<CreditType>) : RecyclerView.A
 
     override fun onBindViewHolder(holder: CreditAdapter.CreditHolder, position: Int) {
         val itemCredit = crediti[position]
-        holder.bindCredit(itemCredit)
+        holder.bind(itemCredit)
     }
 
     override fun getItemCount() = crediti.size
@@ -35,22 +36,41 @@ class CreditAdapter(private val crediti: ArrayList<CreditType>) : RecyclerView.A
 
         private var view: View = v
         private var credito: CreditType? = null
+        private var isExpanded = false
         init {
             v.setOnClickListener(this)
         }
-
-
-        override fun onClick(v: View) {
-            Log.d("RecyclerView", "CLICK!")
-        }
-
-        fun bindCredit(credit: CreditType) {
+        fun bind(credit: CreditType) {
             this.credito = credit
             /* Cambiare i campi */
             view.textViewGeneralita.text = credit.generalita
             view.textViewDescr.text = credit.descrizione
             view.textViewSoldi.text = "€" + credit.soldi.toString()
+            view.setOnClickListener {
+                if (isExpanded) collapse() else expand()
+            }
+            //All tasks should be collapsed by default
+            collapse()
         }
+
+        override fun onClick(v: View) {
+            Log.d("RecyclerView", "CLICK!")
+        }
+
+        private fun expand()
+        {
+            AnimationUtils.expand(itemView.CL_card_credit)
+            //itemView.dropdownButton.setImageResource(R.drawable.ic_keyboard_arrow_up_black_24dp)
+            isExpanded = true
+        }
+        private fun collapse()
+        {
+            AnimationUtils.collapse(itemView.CL_card_credit)
+            //itemView.dropdownButton.setImageResource(R.drawable.ic_keyboard_arrow_down_black_24dp)
+            isExpanded = false
+        }
+
+
 
         companion object {
             private val CREDIT_KEY = "CREDIT"
