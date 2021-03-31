5a7a8a3f7bca567d000560f5935f9b8faefcbac6
omarTornaghi <omar.tor2011@gmail.com>
2021-03-31 13:30:59 +0200

    Creata top bar activity di inserimento

--- app/src/main/java/com/example/quickbalance/NuovaOpActivity.kt
+++ app/src/main/java/com/example/quickbalance/NuovaOpActivity.kt
@@ -1,11 +1,18 @@
 package com.example.quickbalance
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_creazione_stepuno.*
 
 class NuovaOpActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_creazione_stepuno)
+
+        topAppBar.setNavigationOnClickListener(navigationIconOnClickListener)
+    }
+    val navigationIconOnClickListener= View.OnClickListener {
+        finish()
     }
 }
