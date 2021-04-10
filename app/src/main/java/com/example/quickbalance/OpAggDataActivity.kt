4d1229bb33cbfb62250c71797bb70dd5b4d72408
omarTornaghi <omar.tor2011@gmail.com>
2021-04-10 18:42:59 +0200

    Spinner dinamico rimosso, al suo posto switch

--- app/src/main/java/com/example/quickbalance/OpAggDataActivity.kt
+++ app/src/main/java/com/example/quickbalance/OpAggDataActivity.kt
@@ -3,74 +3,44 @@ package com.example.quickbalance
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.KeyEvent
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
+import androidx.core.content.res.ResourcesCompat
 import kotlinx.android.synthetic.main.activity_op_agg_data.*
-import kotlinx.android.synthetic.main.activity_op_agg_data.spinnerView
 
 class OpAggDataActivity : AppCompatActivity() {
-    lateinit var cdSpinnerAdapter: ArrayAdapter<CharSequence>
-    var setSpinner:Boolean = false
-    var posSelez:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_op_agg_data)
-        cdSpinnerAdapter = ArrayAdapter.createFromResource(this, R.array.spinnerData, R.layout.custom_spinner_edittext_green_layout)
-        cdSpinnerAdapter.setDropDownViewResource(R.layout.custom_spinnercd_dropdown_layout);
-        spinnerView.setAdapter(cdSpinnerAdapter)
         editTextDescrizione.setOnFocusChangeListener(editTextDescrizioneFocusListener)
         editTextimporto.setOnFocusChangeListener(editTextImportoFocusListener)
-        spinnerView.setOnTouchListener(spinnerOnTouchListener)
-        spinnerView.setOnKeyListener(spinnerOnKeyListener)
-        spinnerView.onItemSelectedListener = object :
-            AdapterView.OnItemSelectedListener{
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-            }
-
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                Log.d("XXXX", "SetSPinner: " + setSpinner)
-                if(setSpinner){
-                    if(position == 0)
-                        cdSpinnerAdapter = ArrayAdapter.createFromResource(this@OpAggDataActivity, R.array.spinnerData, R.layout.custom_spinner_edittext_green_layout)
-                    else
-                        cdSpinnerAdapter = ArrayAdapter.createFromResource(this@OpAggDataActivity, R.array.spinnerData, R.layout.custom_spinner_edittext_red_layout)
-                    cdSpinnerAdapter.setDropDownViewResource(R.layout.custom_spinnercd_dropdown_layout)
-                    Log.d("XXXX", "Posizione considerata: " + position)
-                    Log.d("XXXX", "Posizione salvata: " + posSelez)
-                    if(posSelez != position) {
-                        setSpinner = false
-                        spinnerView.setSelection(position, false)
-                        posSelez = position;
-                        spinnerView.setAdapter(cdSpinnerAdapter)
-                    }
-                }
-                else
-                    spinnerView.setSelection(posSelez, true)
-                setSpinner = false
-
-            }
-
-            }
+        toggleButton.setOnClickListener(toggleButtonOnClickListener)
+        creditoToggleButton.setOnClickListener{setToggleCredito()}
+        debitoToggleButton.setOnClickListener({setToggleDebito()})
+        //TODO Set toggle in base a chi ha chiamato l'operazione
+        setToggleDebito()
     }
 
-    private val spinnerOnTouchListener = View.OnTouchListener{ view, event ->
-        setSpinner = true;
-        false
+    private val toggleButtonOnClickListener = View.OnClickListener { view ->
+        if(creditoToggleButton.isChecked)
+            setToggleDebito()
+        else
+            setToggleCredito()
     }
 
-    private val spinnerOnKeyListener = View.OnKeyListener{ view: View, i: Int, keyEvent: KeyEvent ->
-        setSpinner = true
-        false
+    private fun setToggleCredito(){
+        toggleButton.setBackgroundResource(R.drawable.background_switch_green)
+        creditoToggleButton.setTextColor(ResourcesCompat.getColor(getResources(), R.color.green, null))
+        debitoToggleButton.setTextColor(ResourcesCompat.getColor(getResources(), R.color.white, null))
+        creditoToggleButton.isChecked = true
     }
 
-    private fun setSpinner(res:Int) {
-        /* Set dello spinner */
-        cdSpinnerAdapter = ArrayAdapter.createFromResource(this, R.array.spinnerData, res)
-        cdSpinnerAdapter.setDropDownViewResource(R.layout.custom_spinnercd_dropdown_layout);
-        spinnerView.setAdapter(cdSpinnerAdapter)
+    private fun setToggleDebito(){
+        toggleButton.setBackgroundResource(R.drawable.background_switch_red)
+        creditoToggleButton.setTextColor(ResourcesCompat.getColor(getResources(), R.color.white, null))
+        debitoToggleButton.setTextColor(ResourcesCompat.getColor(getResources(), R.color.red, null))
+        debitoToggleButton.isChecked = true
     }
+
 
     private val editTextDescrizioneFocusListener =
         View.OnFocusChangeListener { view, gainFocus ->
