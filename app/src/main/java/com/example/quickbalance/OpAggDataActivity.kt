eed54cba183f3c35f817ae7991ca300b5d9c461e
omarTornaghi <omar.tor2011@gmail.com>
2021-04-11 14:57:40 +0200

    Aggiunto datepicker data inizio

--- app/src/main/java/com/example/quickbalance/OpAggDataActivity.kt
+++ app/src/main/java/com/example/quickbalance/OpAggDataActivity.kt
@@ -1,15 +1,24 @@
 package com.example.quickbalance
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import kotlinx.android.synthetic.main.activity_creazione_stepuno.*
 import kotlinx.android.synthetic.main.activity_op_agg_data.*
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class OpAggDataActivity : AppCompatActivity() {
     var statoToggle:Boolean = false
+    val formatoData:String = "dd/MM/yyyy"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_op_agg_data)
@@ -17,17 +26,64 @@ class OpAggDataActivity : AppCompatActivity() {
         editTextimporto.setOnFocusChangeListener(editTextImportoFocusListener)
         toggleButton.setOnClickListener(toggleButtonOnClickListener)
         creditoToggleButton.setOnClickListener{setToggleCredito()}
-        debitoToggleButton.setOnClickListener({setToggleDebito()})
+        debitoToggleButton.setOnClickListener({ setToggleDebito() })
+        //Datepicker dataInizio
+        val calendar: Calendar = Calendar.getInstance()
+        val dataInizio =
+            OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, monthOfYear)
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateLabelDatePicker(calendar, editTextDataInizio)
+            }
+        editTextDataInizio.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                editTextDataInizio.setCompoundDrawablesWithIntrinsicBounds(
+                    R.drawable.ic_baseline_calendar_green_icon,
+                    0,
+                    0,
+                    0
+                )
+                DatePickerDialog(
+                    this@OpAggDataActivity, dataInizio, calendar
+                        .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+        })
+        //fine datepicker dataInizio
         if(savedInstanceState != null){
             statoToggle = savedInstanceState.getBoolean("statoToggle")
             if(statoToggle == true) setToggleCredito() else setToggleDebito()
             editTextDescrizione.setText(savedInstanceState.getString("editTextDescrizione"))
             editTextimporto.setText(savedInstanceState.getString("editTextImporto"))
+            editTextDataInizio.setText(savedInstanceState.getString("editTextDataInizio"))
         }
         else {
             //TODO Set toggle in base a chi ha chiamato l'operazione
             setToggleCredito()
+            val sdf = SimpleDateFormat(formatoData)
+            editTextDataInizio.setText(sdf.format(Date()))
         }
+    }
+
+    fun getCurrentLocale(context: Context): Locale? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            context.getResources().getConfiguration().getLocales().get(0)
+        } else {
+            context.getResources().getConfiguration().locale
+        }
+    }
+
+    private fun updateLabelDatePicker(myCalendar: Calendar, editText:EditText) {
+        val sdf = SimpleDateFormat(formatoData, getCurrentLocale(this))
+        editText.setText(sdf.format(myCalendar.getTime()))
+        editText.setCompoundDrawablesWithIntrinsicBounds(
+            R.drawable.ic_baseline_calendar_gray_icon,
+            0,
+            0,
+            0
+        )
     }
 
     private val toggleButtonOnClickListener = View.OnClickListener { view ->
@@ -39,35 +95,72 @@ class OpAggDataActivity : AppCompatActivity() {
 
     private fun setToggleCredito(){
         toggleButton.setBackgroundResource(R.drawable.background_switch_green)
-        creditoToggleButton.setTextColor(ResourcesCompat.getColor(getResources(), R.color.green, null))
-        debitoToggleButton.setTextColor(ResourcesCompat.getColor(getResources(), R.color.white, null))
+        creditoToggleButton.setTextColor(
+            ResourcesCompat.getColor(
+                getResources(),
+                R.color.green,
+                null
+            )
+        )
+        debitoToggleButton.setTextColor(
+            ResourcesCompat.getColor(
+                getResources(),
+                R.color.white,
+                null
+            )
+        )
         creditoToggleButton.isChecked = true
         statoToggle = true
     }
 
     private fun setToggleDebito(){
         toggleButton.setBackgroundResource(R.drawable.background_switch_red)
-        creditoToggleButton.setTextColor(ResourcesCompat.getColor(getResources(), R.color.white, null))
+        creditoToggleButton.setTextColor(
+            ResourcesCompat.getColor(
+                getResources(),
+                R.color.white,
+                null
+            )
+        )
         debitoToggleButton.setTextColor(ResourcesCompat.getColor(getResources(), R.color.red, null))
         debitoToggleButton.isChecked = true
         statoToggle = false
     }
 
-
     private val editTextDescrizioneFocusListener =
         View.OnFocusChangeListener { view, gainFocus ->
             if (gainFocus) {
-                editTextDescrizione.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_description_green_icon, 0, 0, 0)
+                editTextDescrizione.setCompoundDrawablesWithIntrinsicBounds(
+                    R.drawable.ic_baseline_description_green_icon,
+                    0,
+                    0,
+                    0
+                )
             } else {
-                editTextDescrizione.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_description_gray_icon, 0, 0, 0)
+                editTextDescrizione.setCompoundDrawablesWithIntrinsicBounds(
+                    R.drawable.ic_baseline_description_gray_icon,
+                    0,
+                    0,
+                    0
+                )
             }
         }
     private val editTextImportoFocusListener =
         View.OnFocusChangeListener { view, gainFocus ->
             if (gainFocus) {
-                editTextimporto.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_euro_green_icon, 0, 0, 0)
+                editTextimporto.setCompoundDrawablesWithIntrinsicBounds(
+                    R.drawable.ic_baseline_euro_green_icon,
+                    0,
+                    0,
+                    0
+                )
             } else {
-                editTextimporto.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_euro_gray_icon, 0, 0, 0)
+                editTextimporto.setCompoundDrawablesWithIntrinsicBounds(
+                    R.drawable.ic_baseline_euro_gray_icon,
+                    0,
+                    0,
+                    0
+                )
             }
         }
 
@@ -76,5 +169,6 @@ class OpAggDataActivity : AppCompatActivity() {
         outState.putBoolean("statoToggle", statoToggle)
         outState.putString("editTextDescrizione", editTextDescrizione.text.toString())
         outState.putString("editTextImporto", editTextimporto.text.toString())
+        outState.putString("editTextDataInizio", editTextDataInizio.text.toString())
     }
 }
